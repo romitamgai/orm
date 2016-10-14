f353daf8b474a64ca9e74ce9838111904331a38e
romitamgai <rom_amgai@hotmail.com>
2016-10-14 18:57:44 +0545

    Query built using knex

--- src/controllers/EmployeeController.js
+++ src/controllers/EmployeeController.js
@@ -2,35 +2,34 @@
  * Created by romit on 10/13/16.
  */
 
-import queryUtils from '../utils/QueryUtils';
+import queryUtils from '../utils/QueryUtils'
+import Knex from 'knex';
+import wordMap from '../utils/wordMap';
 
 class EmployeeController {
     constructor(dbUtils) {
         this.databaseUtils = dbUtils;
         this.getAll = this.getAll.bind(this);
+        this.knex = new Knex({client: 'mysql'});
     }
 
     getAll(req, res) {
         const queryParams = req.query;
-        let options = [];
+        let query=this.knex('employee');
         if (queryParams) {
             for (let field of Object.keys(queryParams)) {
-                let queryOption, jsonString;
+                let jsonString;
                 if (typeof queryParams[field] === 'object') {
                     const option = Object.keys(queryParams[field]).pop();
                     jsonString = {field: field, option: option, value: queryParams[field][option]};
-                    queryOption = queryUtils.parseOptionQuery(jsonString);
+                    queryUtils.parseOptionQuery(jsonString, query);
                 } else {
                     jsonString = {field: field, value: queryParams[field]};
-                    queryOption = queryUtils.parseNoOptionQuery(jsonString);
+                    queryUtils.parseNoOptionQuery(jsonString, query);
                 }
-                options.push(queryOption);
             }
         }
-        const tableName = 'employee';
-        let query = queryUtils.buildGetQuery(tableName, options);
-        console.log(query);
-        this.databaseUtils.executeQuery(query).then((response)=> {
+        this.databaseUtils.executeQuery(query.toString()).then((response)=> {
             res.json(response);
         });
     }
--- src/utils/QueryUtils.js
+++ src/utils/QueryUtils.js
@@ -2,47 +2,27 @@
  * Created by romit on 10/14/16.
  */
 import wordMap from './wordMap';
-import _ from 'lodash';
 
 class QueryUtils {
-    static parseOptionQuery(jsonString) {
+
+    static parseOptionQuery(jsonString, query) {
         if (jsonString.field == 'sort') {
-            return `order by ${jsonString.value} ${jsonString.option} `;
+            query.orderBy(jsonString.value, jsonString.option);
         }
-        if (wordMap.hasOwnProperty(jsonString.option)) {
+        else if (wordMap.hasOwnProperty(jsonString.option)) {
             jsonString.option = wordMap[jsonString.option];
         }
-        return `where ${jsonString.field} ${jsonString.option} '${jsonString.value}' `;
+        query.where(jsonString.field, jsonString.option, jsonString.value);
     }
 
-    static parseNoOptionQuery(jsonString) {
-        return QueryUtils.hasStartOrOffset(jsonString) ? `${jsonString.field} ${jsonString.value} ` : `where ${jsonString.field} = '${jsonString.value}' `;
-    }
-
-    static hasStartOrOffset(jsonString) {
+    static parseNoOptionQuery(jsonString, query) {
         if (jsonString.field == 'start') {
-            jsonString.field = 'offset';
-            return true;
+            query.offset(parseInt(jsonString.value));
+        } else if (jsonString.field == 'offset') {
+            query.limit(parseInt(jsonString.value));
+        } else {
+            query.where(jsonString.field, jsonString.value);
         }
-        if (jsonString.field == 'offset') {
-            jsonString.field = 'limit';
-            return true;
-        }
-        return false;
-    }
-
-    static buildGetQuery(tableName, options) {
-        let mappedOptions = options.map((elem, whereCount = 0)=> {
-            if (elem.indexOf('where') == 0) {
-                whereCount++;
-            }
-            if (whereCount > 1) {
-                elem = elem.replace('where', 'and');
-            }
-            return elem;
-        });
-        let queryOptions = mappedOptions.join("");
-        return `SELECT * from ${tableName} ${queryOptions}`;
     }
 }
 
